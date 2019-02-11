33623c437d8dce83a79b983e493e624cbbcac03a
re-is <33784291+re-is@users.noreply.github.com>
2019-02-11 23:10:13 +0100

    Update shl.181214.index.js

--- shl.181214.index.js
+++ shl.181214.index.js
@@ -1,1 +1,1 @@
-document.write('<script type="text/javascript" class="shl-html-class" src="https://rawgit.com/re-is/shl/master/shl.181214.min.js?_=' + (new Date().getTime()) + '"></script>');
+document.write('<script type="text/javascript" class="shl-html-tags" src="https://rawgit.com/re-is/shl/master/shl.181214.min.js?_=' + (new Date().getTime()) + '"></script>');
